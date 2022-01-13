3ec34fda248952d91a9f5eeba8ba077342be9a93
ppaneri <ppaneri@tillster.com>
2022-01-13 18:33:54 +0530

    removed unused import

--- src/main/java/com/inventory/inventory/controller/ArticleController.java
+++ src/main/java/com/inventory/inventory/controller/ArticleController.java
@@ -3,7 +3,6 @@ package com.inventory.inventory.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
